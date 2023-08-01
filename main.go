d210a16bdc754509e0e011b85fb309d7e57ccd5f
Michael Böckli <michael.boeckli@gmail.com>
2023-08-01 20:46:31 +0200

    clean up main

--- main.go
+++ main.go
@@ -16,32 +16,19 @@ var Version string = "dev"
 
 type userFlags struct {
 	outFile string
-	pkgName string
-	// structName string
-	// formatter  string
-	// stubImpl   bool
-	// skipEnsure bool
-	// withResets bool
-	remove bool
-	args   []string
+	remove  bool
+	args    []string
 }
 
 func main() {
 	var flags userFlags
 	flag.StringVar(&flags.outFile, "out", "", "output file (default stdout)")
 	// flag.StringVar(&flags.formatter, "fmt", "", "go pretty-printer: gofmt, goimports or noop (default gofmt)")
-	// flag.StringVar(&flags.structName, "struct", "", "which struct")
-	// flag.BoolVar(&flags.stubImpl, "stub", false,
-	// 	"return zero values when no mock implementation is provided, do not panic")
 	printVersion := flag.Bool("version", false, "show the version for moq")
-	// flag.BoolVar(&flags.skipEnsure, "skip-ensure", false,
-	// 	"suppress mock implementation check, avoid import cycle if mocks generated outside of the tested package")
 	flag.BoolVar(&flags.remove, "rm", false, "first remove output file, if it exists")
-	// flag.BoolVar(&flags.withResets, "with-resets", false,
-	// 	"generate functions to facilitate resetting calls made to a mock")
 
 	flag.Usage = func() {
-		fmt.Println(`moq [flags] source-dir interface [interface2 [interface3 [...]]]`)
+		fmt.Println(`opt [flags] source-dir interface [interface2 [interface3 [...]]]`)
 		flag.PrintDefaults()
 		fmt.Println(`Specifying an alias for the mock is also supported with the format 'interface:alias'`)
 		fmt.Println(`Ex: moq -pkg different . MyInterface:MyMock`)
@@ -51,7 +38,7 @@ func main() {
 	flags.args = flag.Args()
 
 	if *printVersion {
-		fmt.Printf("moq version %s\n", Version)
+		fmt.Printf("opt version %s\n", Version)
 		os.Exit(0)
 	}
 
